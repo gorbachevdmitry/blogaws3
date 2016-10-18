3beb037422ca8b91d8c4252c15fb3b57afd023b0
gorbachevdmitry <gorbachevdmitr@gmail.com>
2016-10-18 12:56:25 +0300

    add dm

--- blog/management/commands/createsu.py
+++ blog/management/commands/createsu.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        if not User.objects.filter(username="dm").exists():
+            User.objects.create_superuser("dm", "gorbachevdmitry@gmail.com.com", "dm")
