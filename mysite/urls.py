8a8974b3fabd57fd0891a3a9e41a1105f1d1c8c2
gorbachevdmitry <gorbachevdmitr@gmail.com>
2016-10-18 15:17:00 +0300

    add s3

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -22,3 +25,7 @@ urlpatterns = [
                            namespace='blog',
                            app_name='blog')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
